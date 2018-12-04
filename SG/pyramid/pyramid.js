91ddc15a15aea1101691ce5862eef15aa9971fd7
Melvin Wilson <melvinwilson2012@gmail.com>
2018-12-04 10:44:05 +1000

    Print Pyramid

--- SG/pyramid/pyramid.js
+++ SG/pyramid/pyramid.js
@@ -80,10 +80,36 @@
         console.log(outPut);
     }
     
+  }
+
+  // pyramid(3);
+
+  function pyramidRecur(n, row = 0, level = '') {
+    if(row ===n) {
+        return;
+    }
+
+    if(level.length === 2 * n - 1) {
+        console.log(level);
+        return pyramidRecur(n, row +1,)
+    }
+
+    const midPoint = Math.floor((2*n - 1)/2);
+    let add;
+
+    if(midPoint - row <= level.length && midPoint + row >= level.length) {
+        add = "#";
+    } else {
+        add = " ";
+    }
+
+    pyramidRecur(n, row, level + add);
 
   }
 
-  pyramid(3);
+  pyramidRecur(3)
+
+  
 
     // n = 1
     // #
